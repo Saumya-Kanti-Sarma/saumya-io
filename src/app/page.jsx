5a3fa4806895b8d93f32de75f6b4874dec8289d0
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-04-22 13:43:57 +0530

    added my photo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,27 @@
-const page = () => {
-  return (
-    <>
-      HELLO
-    </>
-  )
-}
+import Link from "next/link";
+import styles from "./home.module.css";
 
-export default page
+const Page = () => {
+  return (
+    <div className={styles.videoArea}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={styles.pfpVideo}
+      >
+        <source src="/assets/vdo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className={styles.videoTextContainer}>
+        <Link href={""} className={styles.videoArticletitle}>
+          How I changed from Chemistry to Computer!
+        </Link><br />
+        <Link href={""} className={styles.videoArticledescp}>A journey that took me 3000kms away from my comfort zone...</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
